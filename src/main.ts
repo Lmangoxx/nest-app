19fde68b69c1698985e4a64dbb11386a59327f5c
Lmangoxx <lmango.dogmaster@gmail.com>
2022-04-24 14:08:54 +0800

    perf:处理跨域

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // 处理跨域
+  app.enableCors();
+
+  // 设置模板语言
   app.setViewEngine('hbs');
 
   // 自动解析请求参数
